02cbf818bd24e703b9d1a769aa4f686824321d26
Luke Gladys <lukegladys@gmail.com>
2022-05-30 22:26:46 -0400

    Frontend talking to backend

--- src/Beanstalk.Frontend/Data/PlantServiceClient.cs
+++ src/Beanstalk.Frontend/Data/PlantServiceClient.cs
@@ -1,19 +1,31 @@
+using System.Text.Json;
+
 namespace Beanstalk.Frontend.Data;
 
 public class PlantServiceClient
 {
+    private readonly IConfiguration _configuration;
     private readonly IHttpClientFactory _httpClientFactory;
     private readonly ILogger<PlantServiceClient> _logger;
 
-    public PlantServiceClient(IHttpClientFactory httpClientFactory, ILogger<PlantServiceClient> logger)
+    public PlantServiceClient(IConfiguration configuration,
+        IHttpClientFactory httpClientFactory,
+        ILogger<PlantServiceClient> logger)
     {
+        _configuration = configuration;
         _httpClientFactory = httpClientFactory;
         _logger = logger;
     }
 
-    public Task<IEnumerable<Plant>> GetPlantsAsync()
+    public async Task<List<Plant>> GetPlantsAsync()
     {
         // Create the client
-        HttpClient client = _httpClientFactory.CreateClient();
+        var client = _httpClientFactory.CreateClient();
+        
+        var resultMessage =
+            await client.GetFromJsonAsync<IEnumerable<Plant>>(
+                $"{_configuration.GetConnectionString("BackendBase")}/plants");
+
+        return resultMessage is not null ? resultMessage.ToList() : new List<Plant>();
     }
 }
